df18866b6b1fcbcf317df587215b135b91e8d856
toady <support@8pecxstudios.com>
2016-03-12 20:28:46 +1000

    Add packs for 45.0.1 branch

---
+++ releases/45.0.1/ast.xpi/browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,5 @@
+//@line 4 "c:\builds\moz2_slave\rel-m-beta-w32_rpk_1-000000000\l10n\ast\browser\firefox-l10n.js"
+
+//@line 6 "c:\builds\moz2_slave\rel-m-beta-w32_rpk_1-000000000\l10n\ast\browser\firefox-l10n.js"
+
+pref("general.useragent.locale", "ast");
---
+++ releases/45.0.1/id.xpi/browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,5 @@
+//@line 4 "c:\builds\moz2_slave\rel-m-beta-w32_rpk_6-000000000\l10n\id\browser\firefox-l10n.js"
+
+//@line 6 "c:\builds\moz2_slave\rel-m-beta-w32_rpk_6-000000000\l10n\id\browser\firefox-l10n.js"
+
+pref("general.useragent.locale", "id");
