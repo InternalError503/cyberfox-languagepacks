0a0e2bc7451064a99332dd2c5eb8deef5bea4d8d
toady <support@8pecxstudios.com>
2016-03-18 11:15:09 +1000

    Add packs for 45.0.2 branch

---
+++ releases/45.0.2/zh-CN.xpi/browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,6 @@
+//@line 4 "c:\builds\moz2_slave\rel-m-beta-w32_rpk_10-00000000\l10n\zh-CN\browser\firefox-l10n.js"
+
+//@line 6 "c:\builds\moz2_slave\rel-m-beta-w32_rpk_10-00000000\l10n\zh-CN\browser\firefox-l10n.js"
+
+pref("general.useragent.locale", "zh-CN");
+pref("browser.startup.homepage", "chrome://browser/locale/browser.properties");
