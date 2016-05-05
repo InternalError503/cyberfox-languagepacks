74f94fc1b4b138b2719fe8eeb8354bcc7ff37a7e
toady <support@8pecxstudios.com>
2016-05-06 08:45:42 +1000

    Update .gitignore

--- delete ddg.xml .cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-(set /P appVersion=
-)<%userprofile%\documents\Build\VERSION\SMVER.txt
-set DestinPath=releases\%appVersion%
-
-for /f %%f in ('dir /b "%DestinPath%"') do del "%DestinPath%\%%f\browser\chrome\%%~nf\locale\browser\searchplugins\ddg.xml"
-exit /b %ERRORLEVEL%
