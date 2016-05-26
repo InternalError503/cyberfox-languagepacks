2209348d495d42444c4307b38c41ff2981a149a2
toady <support@8pecxstudios.com>
2016-05-26 11:36:29 +1000

    Add packs for 47.0 branch

---
+++ releases/47.0/be.xpi/browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,6 @@
+//@line 4 "c:\builds\moz2_slave\rel-m-beta_fx_w32_l10n_rpk-000\build\l10n\be\browser\firefox-l10n.js"
+
+//@line 6 "c:\builds\moz2_slave\rel-m-beta_fx_w32_l10n_rpk-000\build\l10n\be\browser\firefox-l10n.js"
+
+pref("general.useragent.locale", "be");
+pref("browser.fixup.alternate.suffix", ".by");
