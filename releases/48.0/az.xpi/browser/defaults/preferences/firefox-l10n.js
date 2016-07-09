34892e11583c2ebbfa7d95d41851e5dd6e85b447
toady <support@8pecxstudios.com>
2016-07-09 15:03:17 +1000

    Add packs for 48.0 branch

---
+++ releases/48.0/az.xpi/browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,8 @@
+//@line 4 "c:\builds\moz2_slave\rel-m-beta_fx_w32_l10n_rpk-000\build\l10n\az\browser\firefox-l10n.js"
+
+//@line 6 "c:\builds\moz2_slave\rel-m-beta_fx_w32_l10n_rpk-000\build\l10n\az\browser\firefox-l10n.js"
+
+//@line 9 "c:\builds\moz2_slave\rel-m-beta_fx_w32_l10n_rpk-000\build\l10n\az\browser\firefox-l10n.js"
+pref("browser.search.geoSpecificDefaults", true);
+
+pref("general.useragent.locale", "az");
