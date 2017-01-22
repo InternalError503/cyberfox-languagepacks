a88209242b2295fc502a69854fd8d9a7a647806a
toady <support@8pecxstudios.com>
2017-01-22 13:16:05 +1000

    Add packs for 51.0 branch

---
+++ releases/51.0/en-US.xpi/browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,8 @@
+//@line 4 "c:\builds\moz2_slave\m-rel-w32-00000000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
+
+//@line 6 "c:\builds\moz2_slave\m-rel-w32-00000000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
+
+//@line 9 "c:\builds\moz2_slave\m-rel-w32-00000000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
+pref("browser.search.geoSpecificDefaults", true);
+
+pref("general.useragent.locale", "en-US");
