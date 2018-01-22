d9d1d252d1251e2943f6d875909f2d7ca4350517
toady <support@8pecxstudios.com>
2018-01-22 11:58:09 +1000

    Update language pack changes for 52.6.1 branch

---
+++ releases/52.6.1/mai.xpi/browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,5 @@
+//@line 4 "c:\builds\moz2_slave\rel-m-beta_fx_w32_l10n_rpk-000\build\l10n\mai\browser\firefox-l10n.js"
+
+//@line 6 "c:\builds\moz2_slave\rel-m-beta_fx_w32_l10n_rpk-000\build\l10n\mai\browser\firefox-l10n.js"
+
+pref("general.useragent.locale", "mai");
