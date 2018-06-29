ba6dff701d848a8571639f8cfecae086cb32e2f2
toady <support@8pecxstudios.com>
2018-06-29 14:01:35 +1000

    Update language pack changes for 52.9.1 branch

---
+++ releases/52.9.1/ru.xpi/browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,8 @@
+//@line 4 "c:\builds\moz2_slave\rel-m-beta_fx_w32_l10n_rpk-000\build\l10n\ru\browser\firefox-l10n.js"
+
+//@line 6 "c:\builds\moz2_slave\rel-m-beta_fx_w32_l10n_rpk-000\build\l10n\ru\browser\firefox-l10n.js"
+
+pref("general.useragent.locale", "ru");
+
+//@line 11 "c:\builds\moz2_slave\rel-m-beta_fx_w32_l10n_rpk-000\build\l10n\ru\browser\firefox-l10n.js"
+pref("browser.search.geoSpecificDefaults", true);
